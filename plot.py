b8f5cff64cc7be1a850f0a797638f5e8ad73528e
Lukasz Niewinski <lukasz13n@gmail.com>
2019-11-25 19:08:33 +0000

    ploting

--- plot.py
+++ plot.py
@@ -1,34 +1,64 @@
+import glob, os
 import numpy as np
 import matplotlib.pyplot as plt
+# 0.1_graph_0_infected_ratios.csv
 
 # TODO: average results for all networks...
 
 # Plot infected ratio per time-step.
 NUMBER_NETWORKS = 10
 
-file_infected_ratio = "output/graph_0_infected_ratios.csv" 
-data = np.loadtxt(file_infected_ratio, delimiter=',')
-num_timesteps = data.shape[0]
 
-all_data = np.zeros((NUMBER_NETWORKS,num_timesteps))
+folder_name = 'output'
 
-for net in range(NUMBER_NETWORKS):
+parameter = 0.0
 
-    file_infected_ratio = "output/graph_" + str(net) + "_infected_ratios.csv" 
-    all_data[net,:] = np.loadtxt(file_infected_ratio, delimiter=',')
-
-
-all_data = np.mean(all_data, axis=0)
-
-print(all_data)
 
 fig = plt.figure()
 fig.set_size_inches(10, 6.0)
 
-X = np.arange(0, 15)
-Y = data[:15]
+os.chdir(folder_name)
+i = 0;
+for file in glob.glob("*.csv"):
+    # check if file name match - if not, skip
+    if not '_graph_' in file:
+        continue
 
-plt.plot(X, Y)
+    # read parameter value
+    new_parameter = float(file[:3])
+
+    # read_data
+    data = np.loadtxt(file, delimiter=',')
+    num_timesteps = data.shape[0]
+
+    if new_parameter != parameter:
+
+        # if not initial iteration get average and plot
+        if parameter != 0.0:
+            average = np.mean(all_data, axis=0)
+            print(average)
+
+            X = np.arange(0, 15)
+            Y = average[:15]
+
+            plt.plot(X, Y)
+
+            # get the average for matrix
+
+        # data per network
+        all_data = np.zeros((NUMBER_NETWORKS, num_timesteps))
+        parameter = new_parameter
+        i = 0
+
+    all_data[i, :] = data
+    i += 1
+
+for net in range(NUMBER_NETWORKS):
+
+    file_infected_ratio = "output/graph_" + str(net) + "_infected_ratios.csv"
+
+
+
 
 plt.xlabel('Time step')
 plt.ylabel('Infected percentage')
