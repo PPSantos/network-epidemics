8ebae064eb48877b4e4d2e73ea991d61eb06d93f
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2019-11-29 11:00:01 +0000

    Updated plots

--- plot.py
+++ plot.py
@@ -407,6 +407,8 @@ def SIS_plot_disease_prevalence():
 def plot_SIS_1():
     """
         Naive SIS model. FIX beta and vary delta.
+
+        "naive_SIS_delta_variation"
     """
     fig = plt.figure()
     fig.set_size_inches(6.5, 4.5)
@@ -459,6 +461,8 @@ def plot_SIS_1():
 def plot_SIS_1_1():
     """
         naive sis: vary delta and show stationary distribution change.
+
+        "naive_SIS_delta_variation"
     """
     NUMBER_OF_NETWORKS = 10
 
@@ -490,23 +494,108 @@ def plot_SIS_1_1():
 
     fig.savefig('img/SIS/naive_SIS_model_delta_variation_stationary_dist.pdf')
 
+def plot_SIS_mu():
+
+    fig = plt.figure()
+    fig.set_size_inches(6.5, 4.5)
+
+    PARAMETERS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    NUMBER_OF_NETWORKS = 10
+    NUM_TIMESTEPS = 25
+
+    DISPLAY_STEPS = 25
+
+    LABELS = []
+
+    scheme = cm.get_cmap("bwr", len(PARAMETERS))
+    colors = scheme(np.linspace(0.0, 1.0, len(PARAMETERS)))
+
+    # Make a user-defined colormap.
+    cm1 = mcol.LinearSegmentedColormap.from_list("MyCmapName",["r","b"])
+
+    # Make a normalizer that will map the time values from
+    # [start_time,end_time+1] -> [0,1].
+    cnorm = mcol.Normalize(vmin=min(PARAMETERS),vmax=max(PARAMETERS))
+
+    for (parameter, p) in zip(PARAMETERS, range(len(PARAMETERS))):
+
+        all_data = np.zeros((NUMBER_OF_NETWORKS, NUM_TIMESTEPS))
+
+        LABELS.append("\u03BC=" + str(parameter))
+
+        for net in range(NUMBER_OF_NETWORKS):
+            file_name = "output/SIS_mu/{0}_graph_{1}_infected_ratios.csv".format(parameter, net)
+            all_data[net,:] = np.loadtxt(file_name, delimiter=',')
+
+        average = np.mean(all_data, axis=0)
+        X = np.arange(0, DISPLAY_STEPS)
+        Y = average[:DISPLAY_STEPS] * 100
+
+        plt.plot(X, Y, color=colors[p])
+
+    # Add baseline.
+    all_data = np.zeros((NUMBER_OF_NETWORKS, NUM_TIMESTEPS))
+
+    LABELS.append("baseline")
+
+    for net in range(NUMBER_OF_NETWORKS):
+        file_name = "output/naive_SIS_delta_variation/0.3_graph_{0}_infected_ratios.csv".format(net)
+        all_data[net,:] = np.loadtxt(file_name, delimiter=',')
+
+    average = np.mean(all_data, axis=0)
+    X = np.arange(0, DISPLAY_STEPS)
+    Y = average[:DISPLAY_STEPS] * 100
+
+    print(Y[-1])
+
+    plt.plot(X, Y, color='black', linestyle='--')
+
+    plt.xlabel('Time step')
+    plt.ylabel('Infected percentage (%)')
+    plt.title("SIS model \n [\u03B3=10, \u03C6=5, \u03B2=0.4, \u0394=0.3, \u03A9=0.2, \u03BB=0.6]")
+    plt.legend(LABELS)
+    plt.grid()
+    plt.show()
+
+    fig.savefig('img/SIS/SIS_model_mu_variation.pdf')
+
+def plot_SIS_mu_2():
+
+    NUMBER_OF_NETWORKS = 10
+    NUM_TIMESTEPS = 25
+    DISPLAY_STEPS = 25
+
+    PARAMETERS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    LABELS = []
+
+    fig = plt.figure()
+    fig.set_size_inches(6.5, 4.5)
+
+    results = np.zeros((len(PARAMETERS)))
+
+    for (delta, i) in zip(PARAMETERS, range(len(PARAMETERS))):
+
+        nets_data = np.zeros((NUMBER_OF_NETWORKS))
+
+        for net in range(NUMBER_OF_NETWORKS):
+            file_name = "output/SIS_mu/{0}_graph_{1}_infected_ratios.csv".format(delta, net)
+            nets_data[net] = np.loadtxt(file_name, delimiter=',')[-1]
+
+        results[i] = np.average(nets_data)
+
+    plt.plot(PARAMETERS, results*100)
+
+    plt.xlabel('Awareness spreading rate (\u03BC)')
+    plt.ylabel('Infected stationary distributon (%)')
+    plt.title("SIS model \n [\u03B3=10, \u03C6=5, \u03B2=0.4, \u0394=0.3, \u03A9=0.2, \u03BB=0.6]")
+
+    plt.ylim([0,100])
+    plt.grid()
+    plt.show()
+
+    fig.savefig('img/SIS/SIS_model_mu_variation_2.pdf')
+
 
 if __name__ == '__main__':
 
-    # plot_graph(num_networks=10, file_type='_infected_ratios', display_steps=25)
-
-    #plot_graph_hardcoded()
-
-    #plot_threshold_timesteps()
-
-    #plot_dIdt()
-
-    #plot_dIdt_2()
-
-    #SIS_plot_infected_ratios()
-
-    #SIS_plot_disease_prevalence()
-
-    #plot_SIS_1()
-
-    plot_SIS_1_1()
+    plot_SIS_mu()
